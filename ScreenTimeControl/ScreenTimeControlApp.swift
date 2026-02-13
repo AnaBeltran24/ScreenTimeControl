5ba0b926600fd617f81cc701d02e24ed6f1ee992
Ana Beltran <anasofiabd@gmail.com>
2026-02-12 23:53:23 -0500

    Initial Commit

---
+++ ScreenTimeControl/ScreenTimeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeControlApp.swift
+//  ScreenTimeControl
+//
+//  Created by Daniel Pachon on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
